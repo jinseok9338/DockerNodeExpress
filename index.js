b08441ad4258645136eafc230489d42453c2a95e
jinseok9338 <jinseok9338@gmail.com>
2021-05-29 06:21:56 +0000

    fifth  day

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD,MONGO_IP,MONGO_PORT } = require("./config/config");
+const app = express();
 
+const mongoURL =`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose
+  .connect(mongoURL,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
+  .then(() =>
+    console.log("Succesfully connectedd to DB")).catch((e) => console.log(e))
+  
+app.get("/", (_req, res) => {
+  res.send("Hello World!!!!");
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+//apt-get install -y npm
+//install npm first then install any other packages
